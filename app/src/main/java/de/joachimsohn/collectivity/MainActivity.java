b02b97e50814ab7609fe6a246b46a954e9731cdc
joachimb <bengt.joachimsohn@bewotec.de>
2020-07-16 16:21:30 +0200

    did some style changes and added a better toolbar

--- app/src/main/java/de/joachimsohn/collectivity/MainActivity.java
+++ app/src/main/java/de/joachimsohn/collectivity/MainActivity.java
@@ -1,47 +1,26 @@
 package de.joachimsohn.collectivity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import de.joachimsohn.collectivity.loader.DataBaseConnector;
 import de.joachimsohn.collectivity.manager.impl.CollectionManager;
 import de.joachimsohn.collectivity.ui.Marker;
-import de.joachimsohn.collectivity.ui.tabs.TabAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int ADD_NOTE_REQUEST = 1;
-    private TabAdapter adapter;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private ItemViewModel itemViewModel;
+    Toolbar toolbar;
 
     private void initGUI() {
+        toolbar = findViewById(R.id.toolbar);
+        //TODO: setContentView(R.layout.collection_view);
         setContentView(R.layout.collection_view);
-
-        //viewPager = findViewById(R.id.viewPager);
-        //tabLayout = findViewById(R.id.tabLayout);
-        //adapter = new TabAdapter(getSupportFragmentManager());
-        // adapter.addFragment(new FastItemOverview(), "Tab 1");
-        //adapter.addFragment(new DetailedItemOverview(), "Tab 2");
-        //viewPager.setAdapter(adapter);
-        //tabLayout.setupWithViewPager(viewPager);
-
-        //setupAddItemButton();
-        // setupRecyclerView();
+        //setSupportActionBar(toolbar);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
